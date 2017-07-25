cbf8f1bfd92a684ced50430b0e3c63cdab70e3b0
Songyang2017 <1546302993@qq.com>
2017-07-25 18:17:20 +0800

    5

---
+++ js/scale.js
@@ -0,0 +1,19 @@
+﻿(function (doc, win) {
+		        var docEle = doc.documentElement;
+		       	var resizeEvent = 'orientationchange' in window ? 'orientationchange' : 'resize';
+		        var recalCulate = function () {
+		            var width = docEle.clientWidth;
+		            docEle.style.fontSize = 100 * (width / 640) + 'px';
+		        };
+		
+		        recalCulate();
+		
+		        if (!doc.addEventListener) return;
+		        win.addEventListener(resizeEvent, recalCulate, false);
+		    })(document, window);
+
+
+
+		    // docEle.style.fontSize = x * (width / 640) + 'px';  1rem=x/2 px
+
+		    // http://pxtoem.com/
